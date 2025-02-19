700245fca660cd45e69739e3f0b29f2b429a068d
HaNguyenMinhTam <127753137+HaNguyenMinhTam@users.noreply.github.com>
2025-02-19 15:05:13 +0700

    Update

--- Lab2_Bai1_PTB1/src/PTB1.java
+++ Lab2_Bai1_PTB1/src/PTB1.java
@@ -9,15 +9,17 @@ public class PTB1 {
 		System.out.print("Nhap vao he so b: ");
 		double b = scanner.nextDouble();
 		
-        if (a == 0) {
-            if (b == 0) {
-                System.out.println("Phuong trinh co vo so nghiem.");
-            } else {
-                System.out.println("Phuong trinh vo nghiem.");
-            }
-        } else {
-            double x = -b / a;
-            System.out.println("Nghiem của phuong trinh la x = " + x);
+        if(a == 0) {
+        	if(b == 0) {
+        		System.out.println("Phuong trinh co vo so nghiem");
+        	}
+        	else {
+        		System.out.println("Phuong trinh vo nghiem");
+        	}
+        }
+        else {
+        	double x = -b / a;
+        	System.out.println("Phuong trinh co nghiem: " + x);
         }
         
         scanner.close();

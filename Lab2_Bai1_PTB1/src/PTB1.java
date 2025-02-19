ab4b00bd84da7b5e806d1949c776ea0d0bfb3fe0
HaNguyenMinhTam <127753137+HaNguyenMinhTam@users.noreply.github.com>
2025-02-19 15:01:19 +0700

    PTB1

---
+++ Lab2_Bai1_PTB1/src/PTB1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class PTB1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		//Nhap vao he so a va b
+		System.out.print("Nhap he so a: ");
+		double a = scanner.nextDouble();
+		System.out.print("Nhap vao he so b: ");
+		double b = scanner.nextDouble();
+		
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phuong trinh co vo so nghiem.");
+            } else {
+                System.out.println("Phuong trinh vo nghiem.");
+            }
+        } else {
+            double x = -b / a;
+            System.out.println("Nghiem của phuong trinh la x = " + x);
+        }
+        
+        scanner.close();
+	}
+}
